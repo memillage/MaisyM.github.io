4afca50b2a236fe5e816f6f0c333a6aaab645b59
memillage <135911580+memillage@users.noreply.github.com>
2023-06-09 16:16:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var mobileMenuBtn = document.querySelector("#mobile-menu-btn");
+
+var mobileMenuBtn = document.querySelector(".hamburger");
 var mobileMenu = document.querySelector(".mobile-menu");
 
 mobileMenuBtn.addEventListener("click", () => {
@@ -14,14 +15,38 @@ mobileMenuBtn.addEventListener("click", () => {
 
 window.onscroll = function() {myFunction()};
 
-var navbar = document.querySelector("nav");
+var navbar = document.querySelector(".navbar");
 var sticky = navbar.offsetTop;
+var navbar2 = document.querySelector("mobile-menu");
+var sticky2 = navbar2.offsetTop;
+
+function openLink(link){
+  if (confirm("Open instagram in new tab?") == true) {
+    window.open("http://"+link);
+  }
+}
+function openMail(){
+    if (confirm("Open email?") == true) {
+        window.open("mailto:maisy.millage@gmail.com");
+      }
+}
+function openNum(){
+    if (confirm("Call: +1 (805)-746-4832 ?") == true) {
+        window.open("tel:+1 (805)-746-4832");
+      }
+}
+
+function playV1(){
+    //window.alert("s1 pressed");
+    $('.v1')[0].contentWindow.postMessage('{"event":"command","func":"'+'playVideo'+'",":""}','*');
+}
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
+    navbar2.classList.add("sticky2")
   } else {
     navbar.classList.remove("sticky");
+    navbar2.classList.remove("sticky2");
   }
 }
-
